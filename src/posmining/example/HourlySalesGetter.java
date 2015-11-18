619459293f1da6f9b1b856b97d59e99c3cf6ae18
medalhkr <medalhkr@gmail.com>
2015-11-18 14:26:56 +0900

    [add] HourlySalesGetterに複数ターゲットを処理する機能を追加
    main関数の最初にターゲット（分類名）を指定する部分があります．
    その他，ユーザが変更する必要がある部分はすべてmain関数の最初になってます．
    また，ローカルでテストする時とEMRでやるときで変更する箇所がある
    （runJobWithParam関数一番下）ので注意してください．

--- src/posmining/example/HourlySalesGetter.java
+++ src/posmining/example/HourlySalesGetter.java
@@ -24,16 +24,40 @@ import posmining.utils.PosUtils;
  */
 public class HourlySalesGetter {
 
-
 	// MapReduceを実行するためのドライバ
 	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+		String jobname = "2015012";
+		Class<HourlySalesGetter> jarclass = HourlySalesGetter.class;
+		String outdir = "Hourly";
+		String[] category_list = {
+				"チルドムース",
+				"フルーツ入りヨーグルト",
+				"チルドプリン",
+				"ドリンクヨーグルト",
+				"半生ケーキ",
+				"常温ゼリー",
+				"チョコレート菓子",
+				};
+
+		String input = args.length > 0 ? args[0] : "posdata";
+		for(String category : category_list){
+			String output = String.format("out/%s/%s", outdir, category);
+			runJobWithParam(category, input, output, jobname, jarclass);
+		}
+	}
+
+	private static void runJobWithParam(String category, String input, String output, String jobname, Class<?> jarclass)
+			 throws IOException, InterruptedException, ClassNotFoundException {
 
 		// MapperクラスとReducerクラスを指定
-		Job job = new Job(new Configuration());
-		job.setJarByClass(HourlySalesGetter.class);       // ★このファイルのメインクラスの名前
+		Configuration conf = new Configuration();
+		conf.set("category", category);
+		Job job = new Job(conf);
+
+		job.setJarByClass(jarclass);
 		job.setMapperClass(MyMapper.class);
 		job.setReducerClass(MyReducer.class);
-		job.setJobName("2015012");                        // ★自分の学籍番号
+		job.setJobName(jobname);
 
 		// 入出力フォーマットをテキストに指定
 		job.setInputFormatClass(TextInputFormat.class);
@@ -45,24 +69,21 @@ public class HourlySalesGetter {
 		job.setOutputKeyClass(CSKV.class);
 		job.setOutputValueClass(CSKV.class);
 
-		// 入出力ファイルを指定
-		String inputpath = "posdata";
-		String outputpath = "out/HourlySales";     // ★MRの出力先
-		if (args.length > 0) {
-			inputpath = args[0];
-		}
-
-		FileInputFormat.setInputPaths(job, new Path(inputpath));
-		FileOutputFormat.setOutputPath(job, new Path(outputpath));
+		FileInputFormat.setInputPaths(job, new Path(input));
+		FileOutputFormat.setOutputPath(job, new Path(output));
 
 		// 出力フォルダは実行の度に毎回削除する（上書きエラーが出るため）
-		PosUtils.deleteOutputDir(outputpath);
+		PosUtils.deleteOutputDir(output);
 
 		// Reducerで使う計算機数を指定
 		job.setNumReduceTasks(8);
-
 		// MapReduceジョブを投げ，終わるまで待つ．
-		job.waitForCompletion(true);
+
+		// waitForだと前のジョブが終わってから次を投入するので，途中でeclipseを終了すると以降のジョブが走らない
+		// submitだと何も待たずに全部突っ込むのでeclipse関係ない
+		// ただしローカル環境でsubmitするとOutOfMemoryなので，ローカルならwaitFor, EMRならsubmitにすればいい
+//		job.waitForCompletion(true); // 待つ
+		job.submit();  // 待たない
 	}
 
 
@@ -74,7 +95,8 @@ public class HourlySalesGetter {
 			String csv[] = value.toString().split(",");
 
 			// ターゲットカテゴリでないレシートは無視
-			if (csv[PosUtils.ITEM_CATEGORY_NAME].equals("チルドプリン") == false) {
+			Configuration conf = context.getConfiguration();
+			if (csv[PosUtils.ITEM_CATEGORY_NAME].equals(conf.get("category")) == false) {
 				return;
 			}
 
