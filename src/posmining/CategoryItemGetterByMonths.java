264599ae0772a44e9d9109c2ced592abfbd6a437
naoya_ln <kumoskar@gmail.com>
2015-11-26 22:53:43 +0900

    月別の修正

--- src/posmining/CategoryItemGetterByMonths.java
+++ src/posmining/CategoryItemGetterByMonths.java
@@ -1,4 +1,4 @@
-package posmining.example;
+package posmining;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
@@ -23,16 +23,40 @@ import posmining.utils.PosUtils;
  */
 public class CategoryItemGetterByMonths {
 
-
 	// MapReduceを実行するためのドライバ
 	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+		String jobname = "2015001";
+		Class<CategoryItemGetterByMonths> jarclass = CategoryItemGetterByMonths.class;
+		String outdir = "CategoryItemGetterByMonths";
+		String[] category_list = {
+				"チルドムース",
+				"フルーツ入りヨーグルト",
+				"チルドプリン",
+				"ドリンクヨーグルト",
+				"半生ケーキ",
+				"常温ゼリー",
+				"チョコレート菓子",
+				};
+
+		String input = args.length > 0 ? args[0] : "posdata";
+		for(String category : category_list){
+			String output = String.format("out/%s/%s", outdir, category);
+			runJobWithParam(category, input, output, jobname, jarclass);
+		}
+	}
+
+	private static void runJobWithParam(String category, String input, String output, String jobname, Class<?> jarclass)
+			 throws IOException, InterruptedException, ClassNotFoundException {
 
 		// MapperクラスとReducerクラスを指定
-		Job job = new Job(new Configuration());
-		job.setJarByClass(CategoryItemGetterByMonths.class);       // ★このファイルのメインクラスの名前
+		Configuration conf = new Configuration();
+		conf.set("category", category);
+		Job job = new Job(conf);
+
+		job.setJarByClass(jarclass);
 		job.setMapperClass(MyMapper.class);
 		job.setReducerClass(MyReducer.class);
-		job.setJobName("2015001");                        // ★自分の学籍番号
+		job.setJobName(jobname);
 
 		// 入出力フォーマットをテキストに指定
 		job.setInputFormatClass(TextInputFormat.class);
@@ -44,26 +68,22 @@ public class CategoryItemGetterByMonths {
 		job.setOutputKeyClass(CSKV.class);
 		job.setOutputValueClass(CSKV.class);
 
-		// 入出力ファイルを指定
-		String inputpath = "posdata";
-		String outputpath = "out/categoryItemGetterByMonths";     // ★MRの出力先
-		if (args.length > 0) {
-			inputpath = args[0];
-		}
-
-		FileInputFormat.setInputPaths(job, new Path(inputpath));
-		FileOutputFormat.setOutputPath(job, new Path(outputpath));
+		FileInputFormat.setInputPaths(job, new Path(input));
+		FileOutputFormat.setOutputPath(job, new Path(output));
 
 		// 出力フォルダは実行の度に毎回削除する（上書きエラーが出るため）
-		PosUtils.deleteOutputDir(outputpath);
+		PosUtils.deleteOutputDir(output);
 
 		// Reducerで使う計算機数を指定
 		job.setNumReduceTasks(8);
-
 		// MapReduceジョブを投げ，終わるまで待つ．
-		job.waitForCompletion(true);
-	}
 
+		// waitForだと前のジョブが終わってから次を投入するので，途中でeclipseを終了すると以降のジョブが走らない
+		// submitだと何も待たずに全部突っ込むのでeclipse関係ない
+		// ただしローカル環境でsubmitするとOutOfMemoryなので，ローカルならwaitFor, EMRならsubmitにすればいい
+//		job.waitForCompletion(true); // 待つ
+		job.submit();  // 待たない
+	}
 
 	// Mapperクラスのmap関数を定義
 	public static class MyMapper extends Mapper<LongWritable, Text, CSKV, CSKV> {
@@ -72,8 +92,9 @@ public class CategoryItemGetterByMonths {
 			// csvファイルをカンマで分割して，配列に格納する
 			String csv[] = value.toString().split(",");
 
-			// チルドプリンでないレシートは無視
-			if (csv[PosUtils.ITEM_CATEGORY_NAME].equals("チルドプリン") == false) {
+			// ターゲットカテゴリでないレシートは無視
+			Configuration conf = context.getConfiguration();
+			if (csv[PosUtils.ITEM_CATEGORY_NAME].equals(conf.get("category")) == false) {
 				return;
 			}
 
@@ -86,7 +107,7 @@ public class CategoryItemGetterByMonths {
 			String price = csv[PosUtils.ITEM_TOTAL_PRICE];
 
 			//emitする （emitデータはCSKVオブジェクトに変換すること）
-			context.write(new CSKV(name+"\t"+month),new CSKV(count+","+price));
+			context.write(new CSKV(name+","+month),new CSKV(count+","+price));
 		}
 	}
 
@@ -95,6 +116,11 @@ public class CategoryItemGetterByMonths {
 	public static class MyReducer extends Reducer<CSKV, CSKV, CSKV, CSKV> {
 		protected void reduce(CSKV key, Iterable<CSKV> values, Context context) throws IOException, InterruptedException {
 
+			String name;
+			String month;
+			String k[] = key.toString().split(",");
+			name = k[0];
+			month = k[1];
 			int total_count=0;
 			long total_price=0;
 			// 売り上げを合計
@@ -105,7 +131,7 @@ public class CategoryItemGetterByMonths {
 			}
 
 			// emit
-			context.write(key, new CSKV(total_count+"\t"+total_price));
+			context.write(new CSKV(name), new CSKV(total_count+"\t"+total_price+"\t"+month));
 		}
 	}
 }
