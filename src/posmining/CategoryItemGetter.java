f932a030dcd8fc0702e9603e3a6ba3991f12e353
今江健悟 <kengo@imaekengo-no-MacBook-Air.local>
2015-11-27 09:55:06 +0900

    [change] CategoryItemGetter.javaを更新

--- src/posmining/CategoryItemGetter.java
+++ src/posmining/CategoryItemGetter.java
@@ -1,4 +1,4 @@
-package posmining.example;
+package posmining;
 
 import java.io.IOException;
 
@@ -18,22 +18,46 @@ import posmining.utils.CSKV;
 import posmining.utils.PosUtils;
 
 /**
- * 入力されたカテゴリの商品名リストを出力する
+ * 入力されたカテゴリの商品名に対応する売り上げと個数のリストを出力する
  * @author kengo92i
  *
  */
 public class CategoryItemGetter {
 
-
 	// MapReduceを実行するためのドライバ
 	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+		String jobname = "2015026";
+		Class<CategoryItemGetter> jarclass = CategoryItemGetter.class;
+		String outdir = "CategoryItemGetter";
+		String[] category_list = {
+				"チルドムース",
+				"フルーツ入りヨーグルト",
+				"チルドプリン",
+				"ドリンクヨーグルト",
+				"半生ケーキ",
+				"常温ゼリー",
+				"チョコレート菓子",
+				};
+
+		String input = args.length > 0 ? args[0] : "posdata";
+		for(String category : category_list){
+			String output = String.format("out/%s/%s", outdir, category);
+			runJobWithParam(category, input, output, jobname, jarclass);
+		}
+	}
+
+	private static void runJobWithParam(String category, String input, String output, String jobname, Class<?> jarclass)
+			 throws IOException, InterruptedException, ClassNotFoundException {
 
 		// MapperクラスとReducerクラスを指定
-		Job job = new Job(new Configuration());
-		job.setJarByClass(CategoryItemGetter.class);       // ★このファイルのメインクラスの名前
+		Configuration conf = new Configuration();
+		conf.set("category", category);
+		Job job = new Job(conf);
+
+		job.setJarByClass(jarclass);
 		job.setMapperClass(MyMapper.class);
 		job.setReducerClass(MyReducer.class);
-		job.setJobName("2015026");                        // ★自分の学籍番号
+		job.setJobName(jobname);
 
 		// 入出力フォーマットをテキストに指定
 		job.setInputFormatClass(TextInputFormat.class);
@@ -45,24 +69,21 @@ public class CategoryItemGetter {
 		job.setOutputKeyClass(CSKV.class);
 		job.setOutputValueClass(CSKV.class);
 
-		// 入出力ファイルを指定
-		String inputpath = "posdata";
-		String outputpath = "out/categoryItemGetter";     // ★MRの出力先
-		if (args.length > 0) {
-			inputpath = args[0];
-		}
-
-		FileInputFormat.setInputPaths(job, new Path(inputpath));
-		FileOutputFormat.setOutputPath(job, new Path(outputpath));
+		FileInputFormat.setInputPaths(job, new Path(input));
+		FileOutputFormat.setOutputPath(job, new Path(output));
 
 		// 出力フォルダは実行の度に毎回削除する（上書きエラーが出るため）
-		PosUtils.deleteOutputDir(outputpath);
+		PosUtils.deleteOutputDir(output);
 
 		// Reducerで使う計算機数を指定
 		job.setNumReduceTasks(8);
-
 		// MapReduceジョブを投げ，終わるまで待つ．
-		job.waitForCompletion(true);
+
+		// waitForだと前のジョブが終わってから次を投入するので，途中でeclipseを終了すると以降のジョブが走らない
+		// submitだと何も待たずに全部突っ込むのでeclipse関係ない
+		// ただしローカル環境でsubmitするとOutOfMemoryなので，ローカルならwaitFor, EMRならsubmitにすればいい
+		//job.waitForCompletion(true); // 待つ
+		job.submit();  // 待たない
 	}
 
 
@@ -73,17 +94,18 @@ public class CategoryItemGetter {
 			// csvファイルをカンマで分割して，配列に格納する
 			String csv[] = value.toString().split(",");
 
-			// おにぎりでないレシートは無視
-			if (csv[PosUtils.ITEM_CATEGORY_NAME].equals("チルドプリン") == false) {
+			// ターゲットカテゴリでないレシートは無視
+			Configuration conf = context.getConfiguration();
+			if (csv[PosUtils.ITEM_CATEGORY_NAME].equals(conf.get("category")) == false) {
 				return;
 			}
 
 			// valueとなる商品名を取得
 			String name = csv[PosUtils.ITEM_NAME];
-			String price = csv[PosUtils.ITEM_TOTAL_PRICE];
+			String sales = csv[PosUtils.ITEM_COUNT] + ":" +  csv[PosUtils.ITEM_TOTAL_PRICE];
 
 			// emitする （emitデータはCSKVオブジェクトに変換すること）
-			context.write(new CSKV(name), new CSKV(price));
+			context.write(new CSKV(name), new CSKV(sales));
 		}
 	}
 
@@ -92,14 +114,18 @@ public class CategoryItemGetter {
 	public static class MyReducer extends Reducer<CSKV, CSKV, CSKV, CSKV> {
 		protected void reduce(CSKV key, Iterable<CSKV> values, Context context) throws IOException, InterruptedException {
 
-			// 売り上げを合計
-			int count = 0;
+			// 合計を計算
+			int total_count = 0;
+			long total_price = 0;  // オーバフロー大丈夫か？単価を出力しといてexcelでトータルのがいいかな
 			for (CSKV value : values) {
-				count += value.toInt();
+				String sales[] = value.toString().split(":");
+				total_count += Integer.parseInt(sales[0]);
+				total_price += Integer.parseInt(sales[1]);
 			}
 
 			// emit
-			context.write(key, new CSKV(count));
+			String out = String.valueOf(total_count) + "\t" + String.valueOf(total_price);
+			context.write(key, new CSKV(out));
 		}
 	}
 }
